140df2f1edcf98d41d4f75b6039671373cb767e0
Wemite <55292216+Wemite@users.noreply.github.com>
2026-03-10 06:58:33 +0300

    Delete build.rs

--- build.rs
+++
@@ -1,9 +0,0 @@
-use cxx_qt_build::{CxxQtBuilder, QmlModule};
-
-fn main() {
-    CxxQtBuilder::new_qml_module(
-        QmlModule::new("SmoothySearch").qml_file("qml/Main.qml"),
-    )
-    .files(["src/backend.rs"])
-    .build();
-}
